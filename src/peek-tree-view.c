77a504de51ea764500030378783984a8b4dc846a
Alex Reynolds <hellport@protonmail.com>
2023-06-04 20:40:45 -0300

    tree view populated with dummy data

--- src/peek-tree-view.c
+++ src/peek-tree-view.c
@@ -19,6 +19,94 @@ struct _PeekTreeView {
 
 G_DEFINE_TYPE (PeekTreeView, peek_tree_view, GTK_TYPE_BIN)
 
+typedef struct {
+  const gchar  *name;
+  const guint   pid;
+  const gdouble memory;
+  const gchar  *user;
+} Process;
+
+enum {
+  COLUMN_NAME,
+  COLUMN_PID,
+  COLUMN_MEMORY,
+  COLUMN_USER,
+  NUM_COLUMNS
+};
+
+static Process data[] = 
+{
+  { "firefox", 2817, 318.9, "alex" },
+  { "code", 2476, 132.6, "alex" },
+  { "dconf-service", 1931, 819.2, "root" },
+};
+
+static void
+peek_tree_view_selection_changed (GtkTreeSelection *selection,
+                                  gpointer          data)
+{
+
+}
+
+static GtkTreeModel *
+peek_tree_view_create_model ()
+{
+  GtkListStore *store;
+  GtkTreeIter   iter;
+
+  store = gtk_list_store_new (NUM_COLUMNS,
+                              G_TYPE_STRING,
+                              G_TYPE_UINT,
+                              G_TYPE_DOUBLE,
+                              G_TYPE_STRING);
+  
+  for (guint i = 0; i < G_N_ELEMENTS (data); i++)
+  {
+    gtk_list_store_append (store, &iter);
+    gtk_list_store_set (store, &iter,
+                        COLUMN_NAME, data[i].name,
+                        COLUMN_PID, data[i].pid,
+                        COLUMN_MEMORY, data[i].memory,
+                        COLUMN_USER, data[i].user,
+                        -1);
+  }
+
+  return GTK_TREE_MODEL (store);
+}
+
+static void
+peek_tree_view_add_columns (GtkTreeView *tree_view)
+{
+  GtkCellRenderer   *renderer;
+  GtkTreeViewColumn *column;
+
+  renderer = gtk_cell_renderer_text_new ();
+
+  column = gtk_tree_view_column_new_with_attributes ("Process Name",
+                                                     renderer,
+                                                     "text", COLUMN_NAME,
+                                                     NULL);
+  gtk_tree_view_append_column (tree_view, column);
+
+  column = gtk_tree_view_column_new_with_attributes ("PID",
+                                                     renderer,
+                                                     "text", COLUMN_PID,
+                                                     NULL);
+  gtk_tree_view_append_column (tree_view, column);
+
+  column = gtk_tree_view_column_new_with_attributes ("Memory",
+                                                     renderer,
+                                                     "text", COLUMN_MEMORY,
+                                                     NULL);
+  gtk_tree_view_append_column (tree_view, column);
+
+  column = gtk_tree_view_column_new_with_attributes ("User",
+                                                     renderer,
+                                                     "text", COLUMN_USER,
+                                                     NULL);
+  gtk_tree_view_append_column (tree_view, column);
+}
+
 static void
 peek_tree_view_finalize (GObject *object)
 {
@@ -29,6 +117,16 @@ static void
 peek_tree_view_init (PeekTreeView *self)
 {
   gtk_widget_init_template (GTK_WIDGET (self));
+
+  self->model = peek_tree_view_create_model ();
+
+  gtk_tree_view_set_model (GTK_TREE_VIEW (self->tree_view), self->model);
+
+  g_object_unref (self->model);
+
+  peek_tree_view_add_columns (GTK_TREE_VIEW (self->tree_view));
+
+  gtk_widget_show_all (GTK_WIDGET (self));
 }
 
 static void
@@ -42,6 +140,9 @@ peek_tree_view_class_init (PeekTreeViewClass *klass)
   gtk_widget_class_bind_template_child (GTK_WIDGET_CLASS (klass),
                                         PeekTreeView,
                                         tree_view);
+  
+  gtk_widget_class_bind_template_callback (GTK_WIDGET_CLASS (klass),
+                                           peek_tree_view_selection_changed);
 }
 
 PeekTreeView *
